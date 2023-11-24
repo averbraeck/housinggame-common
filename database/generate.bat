f96a6776e76fd0a05b0676ff5a056ce123710bb3
Alexander Verbraeck <a.verbraeck@gmail.com>
2023-11-24 23:10:44 +0100

    Example of jOOQ database generation script

---
+++ database/generate.bat
@@ -0,0 +1,10 @@
+set JAVA_HOME=c:\app\jdk-17.0.2
+c:\app\jdk-17.0.2\bin\java -classpath jooq-3.18.6.jar;^
+jooq-meta-3.18.6.jar;^
+jooq-codegen-3.18.6.jar;^
+reactive-streams-1.0.3.jar;^
+r2dbc-spi-1.0.0.RELEASE.jar;^
+jakarta.xml.bind-api-3.0.0.jar;^
+jaxb-api-2.3.1.jar;^
+mysql-connector-java-8.0.25.jar;. ^
+org.jooq.codegen.GenerationTool jooq-config.xml
