4c0a3b1ec59991325c05e8492179a57b9e45bc82
Alexander Verbraeck <a.verbraeck@gmail.com>
2023-12-14 02:18:48 +0100

    Issue #15. HouseGroupStatus indicates availability of a house

---
+++ src/main/java/nl/tudelft/simulation/housinggame/common/HouseGroupStatus.java
@@ -0,0 +1,16 @@
+package nl.tudelft.simulation.housinggame.common;
+
+/**
+ * HouseGroupStatus.java.
+ * <p>
+ * Copyright (c) 2020-2020 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
+ * BSD-style license. See <a href="https://opentrafficsim.org/docs/current/license.html">OpenTrafficSim License</a>.
+ * </p>
+ * @author <a href="https://www.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+ */
+public class HouseGroupStatus
+{
+    public static final String AVAILABLE = "AVAILABLE";
+    public static final String OCCUPIED = "OCCUPIED";
+    public static final String NOT_AVAILABLE = "NOT_AVAILABLE";
+}
--- src/main/java/nl/tudelft/simulation/housinggame/common/TransactionStatus.java
+++ src/main/java/nl/tudelft/simulation/housinggame/common/TransactionStatus.java
@@ -1,14 +1,12 @@
 package nl.tudelft.simulation.housinggame.common;
 
 /**
- * HouseRoundStatus.java.
+ * TransactionStatus.java.
  * <p>
  * Copyright (c) 2020-2020 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
  * BSD-style license. See <a href="https://opentrafficsim.org/docs/current/license.html">OpenTrafficSim License</a>.
  * </p>
  * @author <a href="https://www.tudelft.nl/averbraeck">Alexander Verbraeck</a>
- * @author <a href="https://www.tudelft.nl/pknoppers">Peter Knoppers</a>
- * @author <a href="https://www.transport.citg.tudelft.nl">Wouter Schakel</a>
  */
 public class TransactionStatus
 {
