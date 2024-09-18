cef20096f2bcadcc36a82fea17e76597ea8c6112
Alexander Verbraeck <a.verbraeck@gmail.com>
2024-09-18 08:32:53 +0200

    SqlUtils as the root of other Utils classes

--- src/main/java/nl/tudelft/simulation/housinggame/common/SqlUtils.java
+++ src/main/java/nl/tudelft/simulation/housinggame/common/SqlUtils.java
@@ -12,6 +12,9 @@ import org.jooq.Table;
 import org.jooq.TableField;
 import org.jooq.impl.DSL;
 
+import nl.tudelft.simulation.housinggame.data.Tables;
+import nl.tudelft.simulation.housinggame.data.tables.records.UserRecord;
+
 public class SqlUtils
 {
 
@@ -23,6 +26,24 @@ public class SqlUtils
 
         Class.forName("com.mysql.cj.jdbc.Driver");
         return DriverManager.getConnection(jdbcURL, dbUser, dbPassword);
+    }
+
+    public static UserRecord readUserFromUserId(final CommonData data, final int userId)
+    {
+        DSLContext dslContext = DSL.using(data.getDataSource(), SQLDialect.MYSQL);
+        return dslContext.selectFrom(Tables.USER).where(Tables.USER.ID.eq(userId)).fetchAny();
+    }
+
+    public static UserRecord readUserFromUsername(final CommonData data, final String username)
+    {
+        DSLContext dslContext = DSL.using(data.getDataSource(), SQLDialect.MYSQL);
+        return dslContext.selectFrom(Tables.USER).where(Tables.USER.USERNAME.eq(username)).fetchAny();
+    }
+
+    public static <R extends org.jooq.UpdatableRecord<R>> R readRecordFromId(final CommonData data, final Table<R> table,
+            final int recordId)
+    {
+        return readRecordFromId(data.getDataSource(), table, recordId);
     }
 
     @SuppressWarnings("unchecked")
